e49dc93c1b1139af7621b7527b5bf2e572a46975
Estella Wang <blesswmy@MacBook-Air.local>
2026-02-24 17:33:59 -0500

    test_bootstrap

---
+++ Submissions/BasicImplementation/test_bootstrap.R
@@ -0,0 +1,26 @@
+library(testthat)
+source("bootstrap_sleep.R")
+
+test_that("sleep duration basic case works", {
+  expect_equal(compute_sleep_duration(22, 30), 8)
+})
+
+test_that("sleep duration handles midnight crossing", {
+  expect_equal(compute_sleep_duration(23, 7), 8)
+})
+
+test_that("bootstrap_mean returns correct length", {
+  set.seed(380)
+  x <- c(1,2,3,4,5)
+  boot <- bootstrap_mean(x, B = 200)
+  expect_equal(length(boot), 200)
+})
+
+test_that("bootstrap_ci returns two values", {
+  set.seed(380)
+  x <- rnorm(100)
+  boot <- bootstrap_mean(x, B = 500)
+  ci <- bootstrap_ci(boot)
+  expect_equal(length(ci), 2)
+})
+
