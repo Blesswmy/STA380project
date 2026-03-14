78f2a62ce4efef72f024baaecb9a9bfc5f74c1d5
Estella Wang <blesswmy@MacBook-Air.local>
2026-03-13 20:56:35 -0400

    upload app.R

---
+++ shiny-app/app.R
@@ -0,0 +1,74 @@
+library(shiny)
+library(bslib)
+library(shinycssloaders)
+library(readxl)
+
+source("../Submissions/BasicImplementation/bootstrap.R")
+
+options(spinner.type = 8, spinner.color = "#6990EE")
+
+data <- read_excel("../Data&PackCom/extdata/Marqueze+et+al.xls")
+
+data <- na.omit(data)
+
+weekday_sleep <- data$Uptimew - data$Bedtimew
+weekend_sleep <- data$Uptimef - data$Bedtimef
+
+sleep_duration <- (weekday_sleep * 5 + weekend_sleep * 2) / 7
+
+sleep_dep <- sleep_duration[data$Depression == 1]
+sleep_nodep <- sleep_duration[data$Depression == 0]
+
+
+ui <- page_sidebar(
+
+  theme = bs_theme(version = 5,
+                   bootswatch = "flatly",
+                   "navbar-bg" = "#2C3E50"),
+
+  title = "Bootstrap Estimation of Sleep Duration",
+
+  sidebar = sidebar(
+
+    selectInput("group",
+                "Group",
+                choices = list("Depression" = "dep",
+                               "No Depression" = "nodep"),
+                selected = "dep"),
+
+    selectInput("stat_type",
+                "Statistic to Bootstrap",
+                choices = list("Mean Sleep Duration" = "mean",
+                               "Median Sleep Duration" = "median"),
+                selected = "mean"),
+
+    numericInput("B",
+                 "Number of Bootstrap Samples",
+                 value = 1000),
+
+    sliderInput("conf_level",
+                "Confidence Level",
+                min = 0.90,
+                max = 0.99,
+                value = 0.95),
+
+    width = 400,
+    open = "always"
+  ),
+
+  withSpinner(plotOutput("bootstrap_hist")),
+
+  verbatimTextOutput("summary_text")
+)
+
+
+
+
+
+
+
+
+
+
+
+shinyApp(ui = ui, server = server)
