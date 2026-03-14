cc85ac4adc3269f423d341048a2de95bedc108e0
Leo Hou <leosmac@Leos-MacBook-Air.local>
2026-03-13 22:23:59 -0400

    commit for additional input and server function

--- shiny-app/app.R
+++ shiny-app/app.R
@@ -30,6 +30,12 @@ ui <- page_sidebar(
 
   sidebar = sidebar(
 
+    numericInput("seed",
+                "Setting the seed",
+                value = 1,
+                min = 1,
+                step = 1),
+
     selectInput("group",
                 "Group",
                 choices = list("Depression" = "dep",
@@ -52,6 +58,13 @@ ui <- page_sidebar(
                 max = 0.99,
                 value = 0.95),
 
+    selectInput("plot_color",
+                "Which color would you like for you Histogram?",
+                choices = list("Red" = "red",
+                               "Blue" = "blue",
+                               "Black" = "black"),
+                selected = "blue"),
+
     width = 400,
     open = "always"
   ),
@@ -62,6 +75,38 @@ ui <- page_sidebar(
 )
 
 
+server <- function(input, output) {
+
+  output$bootstrap_hist <- renderPlot({
+
+    set.seed(input$seed)
+
+    if (input$group == "dep") {
+      x <- sleep_dep}
+    else {
+      x <- sleep_nodep}
+
+    if (input$stat_type == "mean") {
+      bootstrap_value <- bootstrap_mean(x, B = input$B)
+      original_stat <- mean(x)
+      stats_name <- "Mean Sleep Duration"}
+    else {bootstrap_value <- bootstrap_median(x, B = input$B)
+      original_stat <- median(x)
+      stats_name <- "Median Sleep Duration"}
+
+    ci <- bootstrap_ci(bootstrap_value, conf_level = input$conf_level)
+
+    hist(bootstrap_value, col = input$plot_color, border = "white",
+         xlab = paste("Values of ",stats_name),
+         main = paste("Bootstrap Distribution of", stats_name))
+
+    ab_lowerCI <- abline(v = ci[1], lwd = 2, lty = 3)
+    ab_stats <- abline(v = original_stat, lwd = 3, lty = 2)
+    ab_upperCI <- abline(v = ci[2], lwd = 2, lty = 3)
+
+  })
+
+}
 
 
 
